56d9ef403d8011693ef7ec81b71172fef8b3ce4f
VitalikVV2119 <52397486+VitalikVV2119@users.noreply.github.com>
2020-01-21 20:47:47 +0200

    Add files via upload

--- pizza/js/main.js
+++ pizza/js/main.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
 	$(".menu-btn").on('click',function(event) {
 		event.preventDefault();
 		$(".menu").toggleClass('active');
+		$(this).toggleClass('menu-btn_active');
 	});
 });
