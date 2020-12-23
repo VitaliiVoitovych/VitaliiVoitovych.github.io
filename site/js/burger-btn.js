06dcceb2c66423dc1472fafd0eea51fa76ea49a4
VitalikVV2119 <vitalikvv2119@gmail.com>
2020-12-23 18:24:19 +0200

    add scripts, add media

---
+++ site/js/burger-btn.js
@@ -0,0 +1,9 @@
+const button = document.querySelector(".header__btn");
+const menu = document.querySelector(".header__menu");
+
+button.onclick = func;
+
+function func(){
+	menu.classList.toggle('active-menu');
+	menu.classList.toggle('deactive-menu');
+}
