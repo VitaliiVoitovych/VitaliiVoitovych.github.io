272427307d36e8a3a5d0ca9f8911fd9dc715c16f
Vitalii Voitovych <vitalikvv2119@gmail.com>
2021-04-13 12:46:33 +0300

    add accordion

--- Diamond/js/script.js
+++ Diamond/js/script.js
@@ -8,5 +8,17 @@ window.addEventListener('scroll', function(){
 	else{
 		header.classList.add('header_shadow')
 	}
-	console.log(scrolledTop)
 });
+
+document.querySelectorAll('.accordion__trigger').forEach((item) => 
+	item.addEventListener('click', () => {
+		const parent = item.parentNode;
+		if(parent.classList.contains('accordion__item_active')){
+			parent.classList.remove('accordion__item_active');
+		}else{
+			document.querySelectorAll('.accordion__item').forEach((child) => 
+				child.classList.remove('accordion__item_active'))
+			parent.classList.add('accordion__item_active');
+		}
+	})
+)
