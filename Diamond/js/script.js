01a3ac9edf3d34637f3d46d921d178eb47943b12
Vitalii Voitovych <vitalikvv2119@gmail.com>
2021-04-14 21:31:30 +0300

    add contact-block

--- Diamond/js/script.js
+++ Diamond/js/script.js
@@ -22,3 +22,9 @@ document.querySelectorAll('.accordion__trigger').forEach((item) =>
 		}
 	})
 )
+
+var today = new Date();
+var year = today.getFullYear()
+
+var el = document.querySelector('.footer__copyright');
+el.innerHTML = '<span class="footer__title title">Vitalii Voitovych &copy;' + year + '</span>';
