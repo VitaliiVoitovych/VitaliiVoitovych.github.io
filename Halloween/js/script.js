04f07318876525b3bf53e4112ddd03a7c0d979ed
Vitalii Voitovych <vitalikvv2119@gmail.com>
2021-05-14 18:57:06 +0300

    fix

--- Halloween/js/script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".header__burger");
-const menu = document.querySelector(".header__menu");
-button.onclick = func;
-
-function func(){
-	menu.classList.toggle("active-menu");
-	menu.classList.toggle("deactive-menu");
-	button.classList.toggle("active-btn")
-}
